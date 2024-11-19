f23fdf95af46ec45ef51d0ee67020cb39306f7c4
Travis Prescott <trpresco@microsoft.com>
2024-11-19 15:02:19 -0800

    More refactoring. Ugh.

--- test/rest-api-diff.test.ts
+++ test/rest-api-diff.test.ts
@@ -1,5 +1,7 @@
 import { fail } from "assert";
 import { it } from "vitest";
+import { getApplicableRules } from "../src/rules/rules.js";
+import { DiffClient, DiffClientConfig } from "../src/diff-client.js";
 
 it("should compare the test file with the config file", async () => {
   fail("Not implemented");
@@ -14,5 +16,18 @@ it("should write flat violations when --flat-violations is set", async () => {
 });
 
 it("should compare two files", async () => {
+  const args = {
+    "group-violations": true,
+  };
+  const config: DiffClientConfig = {
+    lhs: ["test/files/test1a.json"],
+    rhs: ["test/files/test1b.json"],
+    args: args,
+    rules: getApplicableRules(args),
+  };
+  const client = await DiffClient.create(config);
+  client.parse();
+  client.processDiff();
+  client.buildOutput();
   fail("Not implemented");
 });
