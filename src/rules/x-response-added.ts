5ed7f528b5264cc658eff0eef5a5e6b9d351b3ae
Travis Prescott <trpresco@microsoft.com>
2025-02-26 14:44:45 -0800

    Update rules.

--- src/rules/x-response-added.ts
+++ src/rules/x-response-added.ts
@@ -2,13 +2,16 @@ import { Diff } from "deep-diff";
 import { RuleResult } from "./rules.js";
 
 /**
- * Added Enum to RHS
+ * Flags added responses unless it is a default response.
  */
 export function xResponseAddedRule(
   data: Diff<any, any>
 ): RuleResult | undefined {
   if (!data.path || data.path.length !== 5) return;
   if (data.path[3] !== "responses") return;
+  if (data.path[4] === "default") {
+    return RuleResult.NoViolation;
+  }
   if (data.kind === "N") {
     return RuleResult.FlaggedViolation;
   }
