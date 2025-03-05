8a2dd5a9ec0e13d83c4311b1541ea3b56dca9c39
Travis Prescott <trpresco@microsoft.com>
2025-03-05 10:27:25 -0800

    Update xDefaultRemoved rule.

--- src/rules/x-default-removed.ts
+++ src/rules/x-default-removed.ts
@@ -2,7 +2,8 @@ import { Diff } from "deep-diff";
 import { RuleResult } from "./rules.js";
 
 /**
- * A default field was removed.
+ * A default field was removed, unless the default was "false" because
+ * the default for booleans is implicitly false.
  */
 export function xDefaultRemovedRule(
   data: Diff<any, any>
@@ -10,6 +11,10 @@ export function xDefaultRemovedRule(
   if (!data.path) return;
   const lastPath = data.path[data.path.length - 1];
   if (lastPath === "default" && data.kind === "D") {
-    return RuleResult.FlaggedViolation;
+    if (data.lhs === false) {
+      return RuleResult.NoViolation;
+    } else {
+      return RuleResult.FlaggedViolation;
+    }
   }
 }
