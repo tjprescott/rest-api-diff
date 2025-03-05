35302519305a6adad56bc98ed44b0971224125ce
Travis Prescott <trpresco@microsoft.com>
2025-03-05 15:01:28 -0800

    Azure carve out for headers rule.

--- src/rules/x-headers-added.ts
+++ src/rules/x-headers-added.ts
@@ -2,7 +2,9 @@ import { Diff } from "deep-diff";
 import { RuleResult } from "./rules.js";
 
 /**
- * Added Enum to RHS
+ * Added headers to RHS. There's an exception when
+ * "retry-after" and "location" are added to a 200 or 201 response
+ * because these are added by Azure operation templates.
  */
 export function xHeadersAddedRule(
   data: Diff<any, any>
@@ -11,5 +13,16 @@ export function xHeadersAddedRule(
   const lastPaths = data.path.slice(-2);
   if (!lastPaths.includes("headers")) return;
   if (data.kind !== "N") return;
+  // check if data is an object. If so, collect the top-level keys into an array
+  const keys = Object.keys(data.rhs).toSorted();
+  // Azure carve-out. Ignore 200 and 201 responses with "retry-after" and "location" headers added.
+  if (
+    keys.length === 2 &&
+    keys.includes("retry-after") &&
+    keys.includes("location") &&
+    (data.path.includes("200") || data.path.includes("201"))
+  ) {
+    return RuleResult.NoViolation;
+  }
   return RuleResult.FlaggedViolation;
 }
